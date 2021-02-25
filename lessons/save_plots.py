40542f5b0937bb50f707856cc6b515831bac78de
LEMSantos <lucas_m-santos@hotmail.com>
2021-02-25 08:52:06 -0300

    Add - aula sobre como salvar gráficos

---
+++ lessons/save_plots.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2]
+y = [2, 3]
+
+plt.plot(x, y)
+
+# plt.savefig('../figures/figura1.png')
+# plt.savefig('../figures/figura1.pdf')
+plt.savefig('../figures/figura1.png', dpi=300)
